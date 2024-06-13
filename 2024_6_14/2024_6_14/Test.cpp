a2f877a98c2a647888df6a1e9c4cdfa8db7d68d9
XiaoHe-Codes <133529965+XiaoHe-Codes@users.noreply.github.com>
2024-06-13 23:31:49 +0800

    Add files via upload

---
+++ 2024_6_14/2024_6_14/Test.cpp
@@ -0,0 +1,15 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+	int a;
+	int b;
+	int sum;
+	cout << "输入一个数";
+	cin >> a;
+	cout << "再次输入一个数";
+	cin >> b;
+	sum = a + b;
+	cout << "" << a << "+" << b << "=" << sum << endl;
+	system("pause");
+}
