b7868a342587f1d1c175a7a375e80d3e5e93710f
XiaoHe-Codes <133529965+XiaoHe-Codes@users.noreply.github.com>
2024-06-25 22:16:52 +0800

    Add files via upload

---
+++ com2024_6_25/com2024_6_25/test.cpp
@@ -0,0 +1,34 @@
+#include<iostream>
+using namespace std;
+class IX
+{
+public:
+	virtual void Fx1() = 0;
+	virtual void Fx2() = 0;
+};
+
+class IY
+{
+public:
+	virtual void Fy1() = 0;
+	virtual void Fy1() = 0;
+};
+
+class CA : public IX,//Component
+	       public IY
+{
+public:
+	//≥ÈœÛª˘¿‡IX
+	virtual void Fxl() {
+		cout << "Fxl" << endl;
+	}
+	virtual void Fx2() {
+		cout << "Fx2" << endl;
+	}
+	virtual void Fy1() {
+		cout << "Fy1" << endl;
+	}
+	virtual void Fy2() {
+		cout << "Fy2" << endl;
+	}
+};
